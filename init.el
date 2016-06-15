3000545a6a0d536bd163fd4e6e10a9053fbddf3d
golden <golden@mint-pc>
2016-06-15 09:39:52 +0800

    update

--- init.el
+++ init.el
@@ -87,9 +87,7 @@
 ;; “python”: What Python developers use for extension modules
 ;; “java”: The default style for java-mode (see below)
 ;; “user”: When you want to define your own style
-;(setq
-; c-default-style "linux" ;; set style to "linux"
-					; )
+                    ; )
 (custom-set-variables
  '(c-default-style
   (quote
