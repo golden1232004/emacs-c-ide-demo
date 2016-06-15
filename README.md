b68feaa9b46d391a977f843dff79c90512b4a17e
golden <golden@mint-pc>
2016-06-15 11:42:38 +0800

    add doxygen

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Emacs IDE
 
 1. switch window easily
 2. use chinese input with chinese-pyim（TODO）. It is not needed in Mac emacs.
+3. config doxygen: to generate document for c/c++ code.
+
 
--- init.el
+++ init.el
@@ -191,3 +191,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; configure doxygen
+(add-to-list 'load-path "~/.emacs.d/doxymacs")
+(require 'doxymacs)
+;;注释高亮，针对C和C++程序
+(defun my-doxymacs-font-lock-hook ()
+  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
+      (doxymacs-font-lock)))
+(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
+(doxymacs-mode);doxymacs-mode常true
+(add-hook 'c-mode-common-hook 'doxymacs-mode) ;; 启动doxymacs-mode
+(add-hook 'c++-mode-common-hook 'doxymacs-mode) ;; 启动doxymacs-mode
